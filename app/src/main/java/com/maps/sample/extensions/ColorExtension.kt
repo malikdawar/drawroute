8983968cb282ea71baf507250622d694f805e685
Malik Dawar <malik.dawar@couriootechnologies.com>
2020-09-04 19:55:39 +0500

    Color extension for the color manipulation

---
+++ app/src/main/java/com/maps/sample/extensions/ColorExtension.kt
@@ -0,0 +1,20 @@
+package com.maps.sample.extensions
+
+import android.content.Context
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+/**
+ * An Extension to getColorCompat of view via context
+ * @return void
+ * @author Dawar Malik.
+ */
+fun Context.getColorCompat(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
+
+/**
+ * An Extension to getColorCompat via fragment
+ * @return void
+ * @author Dawar Malik.
+ */
+fun Fragment.getColor(@ColorRes colorRes: Int) = ContextCompat.getColor(requireContext(), colorRes)
